b6c3dd07b015ea08b377c132f1c8143ad03e0595
Miguel Esteban <miguel.esteban@e-campus.uab.cat>
2021-05-07 19:17:03 +0200

    Dashboard mock up

--- DB_Scripts/copy_to_table.sh
+++
@@ -1,3 +0,0 @@
-echo "copiem el fitxer $1 a la taula $2"
-
-sudo psql -U root webike -c "\COPY $2 FROM $1 DELIMITER ',';"
--- read_and_extract.py
+++
@@ -1,8 +0,0 @@
-import py7zr
-import os
-for data in ['Estacions','Informacio']:
-    for x in os.listdir(f'datasets/{data}'):
-        archive = py7zr.SevenZipFile(f'datasets/{data}/{x}', mode='r')
-        archive.extractall(path=f"datasets/{data}_csv/")
-        archive.close()
-
