4290c03ec2a41c058dadb3f89f12b36f1c9ff395
cboned <carlos.boned@e-campus.uab.cat>
2021-05-18 00:39:56 +0200

    modified:   db/info_table_creation.sh 	new file:   db/state_table_creation.sh

--- source/db/info_table_creation.sh
+++ source/db/info_table_creation.sh
@@ -3,17 +3,8 @@ echo "Creem taula $1"
 
 sudo psql -U root webike -c "CREATE TABLE $1 (
     station_id smallint,
-    num_bikes smallint,
-    num_bikes_mech smallint,
-    num_bikes_ebike smallint,
-    num_docks_free smallint,
-    datestamp int,
-    is_changing bool,
-    status varchar(10),
-    is_installed bool,
-    is_renting bool,
-    is_returning bool,
-    ttl smallint
+    capacity smallint,
+    last_updated timestamp
     );"
 
 
